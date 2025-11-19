5a9a1afea3129773679481f88052293e727960d6
ChronoVortex <33610911+ChronoVortex@users.noreply.github.com>
2025-11-19 13:22:07 -0800

    suppress warnings from C code

--- ModManager/src/util.rs
+++ ModManager/src/util.rs
@@ -1,3 +1,11 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unnecessary_transmutes)]
+#![allow(unsafe_op_in_unsafe_fn)]
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use std::{env, ffi::CString};
 use directories::BaseDirs;
