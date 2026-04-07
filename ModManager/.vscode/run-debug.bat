215a3f2fffff26302ed00431aa92705118d0063f
ChronoVortex <33610911+ChronoVortex@users.noreply.github.com>
2026-04-07 09:37:40 -0700

    add vs project build to vscode build scripts

--- ModManager/.vscode/run-debug.bat
+++ ModManager/.vscode/run-debug.bat
@@ -1,2 +1,3 @@
+msbuild ..\UTMTInterface\UTMTInterface.sln /p:Configuration=Release
 set RUST_BACKTRACE=1
 cargo run
--- ModManager/.vscode/run-release.bat
+++ ModManager/.vscode/run-release.bat
@@ -1,3 +1,4 @@
+msbuild ..\UTMTInterface\UTMTInterface.sln /p:Configuration=Release
 if not exist "target\release" mkdir target\release
 for %%f in (*.dll) do copy %%f target\release
 copy ModManLib.runtimeconfig.json target\release
